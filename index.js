f8b911ee792f6b3528be6cf966c9d83eb47ff031
ujee1 <tayakron62@gmail.com>
2024-08-10 21:58:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// const port = 223.206.251.195;
+
+// Route หลักที่ root
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Route สำหรับการดึงข้อมูลผู้ใช้
+app.get('/users', (req, res) => {
+  const users = [
+    { id: 1, name: 'MSU' },
+    { id: 2, name: 'IT-MSU' }
+  ];
+  res.json(users);
+});
+
+// เริ่มต้นเซิร์ฟเวอร์
+app.listen(port,  () => { // '0.0.0.0' หมายถึงฟังทุก interface รวมถึง external IP
+    console.log(`Server is running on http://localhost:${port}`);
+  });
